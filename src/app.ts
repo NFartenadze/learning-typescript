e5556c6af16151f9b5c35d68b9bfc1982faf945d
NFartenadze <nfartenadze@solvd.com>
2023-08-14 13:29:50 +0400

    classes

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+class Department {
+  public name: string;
+  private employees: string[] = [];
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    // validation etc
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+const accounting = new Department("Accounting");
+
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
+accounting.describe();
+accounting.name = "NEW NAME";
+accounting.printEmployeeInformation();
