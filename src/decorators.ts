4939d1843269e6f15b0c094ea798b0b3b304a5d7
NFartenadze <nfartenadze@solvd.com>
2023-08-15 13:45:08 +0400

    different decorators

--- src/decorators.ts
+++ src/decorators.ts
@@ -27,3 +27,55 @@ class PersonWithDecorator {
 
 const person = new PersonWithDecorator();
 console.log(person);
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("property declared");
+  console.log(target, propertyName);
+}
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("accessor Descriptor");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method Decorator");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log("Parameter Decorator");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Wrong Price");
+    }
+  }
+
+  constructor(title: string, price: number) {
+    this.title = title;
+    this._price = price;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this.price * (1 + tax);
+  }
+}
